43232cc1b4a46ed3b1d2e76f65d9fbba6f850e9f
commercium-sys <system@commercium.systems>
2024-01-03 17:39:25 -0500

    boilerplate for Safe process

--- modules/safe/safe/src/lib.rs
+++ modules/safe/safe/src/lib.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
-use uqbar_process_lib::{ await_message, call_init, println, Address, Message, };
+use uqbar_process_lib::{ 
+    await_message, call_init, http, println, 
+    Address, Message, 
+};
 use uqbar_process_lib::eth::EthAddress;
 use std::collections::{ HashMap, HashSet, };
 
@@ -45,6 +48,14 @@ struct State {
 
 fn init (our: Address) {
 
+    let mut state = State {
+        safes: HashMap::new(),
+        peers: HashSet::new(),
+    };
+
+    http::bind_http_path("/", true, false).unwrap();
+    http::bind_ws_path("/", true, false).unwrap();
+
     println!("Hello from Safe! {:?}", our);
 
     loop {
@@ -53,7 +64,7 @@ fn init (our: Address) {
                 println!("Error: {:?}", e);
                 continue;
             }
-            Ok(msg) => match handle_request(&our, &msg) {
+            Ok(msg) => match handle_request(&our, &msg, &mut state) {
                 Ok(()) => continue,
                 Err(e) => println!("Error: {:?}", e),
             },
@@ -62,9 +73,83 @@ fn init (our: Address) {
 
 }
 
-fn handle_request(our: &Address, message: &Message) -> anyhow::Result<()> {
+fn handle_request(our: &Address, msg: &Message, state: &mut State) -> anyhow::Result<()> {
 
-    println!("message: {:?}", message);
+    if !msg.is_request() {
+        return Ok(());
+    }
+
+    if msg.source().node != our.node {
+        handle_p2p_request(msg);
+    } else if
+        msg.source().node == our.node && 
+        msg.source().process == "terminal:terminal:uqbar" {
+            handle_terminal_request(msg);
+
+    } else if 
+        msg.source().node == our.node &&
+        msg.source().process == "http:sys:uqbar" {
+            handle_http_request(our, msg, state);
+
+    }
+
+    println!("message: {:?}", msg);
+
+    Ok(())
+
+}
+
+fn handle_p2p_request(msg: &Message) -> anyhow::Result<()> {
+    println!("p2p message: {:?}", msg);
+    Ok(())
+}
+
+fn handle_terminal_request(msg: &Message) -> anyhow::Result<()> {
+    println!("terminal message: {:?}", msg);
+    Ok(())
+}
+
+fn handle_http_request(our: &Address, msg: &Message, state: &mut State) -> anyhow::Result<()> {
+    println!("http message: {:?}", msg);
+
+    match serde_json::from_slice::<http::HttpServerRequest>(msg.ipc())? {
+        http::HttpServerRequest::Http(ref incoming) => {
+            match handle_http_methods(our, state, incoming) {
+                Ok(()) => Ok(()),
+                Err(e) => {
+                    http::send_response(
+                        http::StatusCode::SERVICE_UNAVAILABLE,
+                        None,
+                        "Service Unavailable".to_string().as_bytes().to_vec(),
+                    )
+                }
+            }
+        }
+        http::HttpServerRequest::WebSocketOpen { path, channel_id } => {
+            Ok(())
+        }
+        http::HttpServerRequest::WebSocketClose(channel_id) => {
+            Ok(())
+        }
+        http::HttpServerRequest::WebSocketPush { .. } => Ok(())
+    }
+
+}
+
+fn handle_http_methods(
+    our: &Address, 
+    state: &mut State, 
+    http_request: &http::IncomingHttpRequest
+) -> anyhow::Result<()> {
+
+    match http_request.method.as_str() {
+        // on GET: give the frontend all of our active games
+        "GET" => Ok(()),
+        "POST" => Ok(()),
+        "PUT" => Ok(()),
+        "DELETE" => Ok(()),
+        _ => http::send_response(http::StatusCode::METHOD_NOT_ALLOWED, None, vec![]),
+    };
     Ok(())
 
 }
